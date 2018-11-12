363b02f3a63939760994f200cdc2260fe4f828e8
Airscript <francesco@airscript.it>
2018-11-12 18:48:10 +0100

    refactor(jaads-cr): minor improvements into cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -79,4 +79,4 @@ function roundToTwo(num) {
 }
 
 /* Function call */
-// console.log(`Outcome: ${checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])}`);
+console.log(`Outcome: ${checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])}`);
