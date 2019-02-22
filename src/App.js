974bcc1d3b4580c722674c66cd3ffb975edd0fcd
Airscript <francesco@airscript.it>
2019-02-22 17:36:01 +0100

    chore(fedl-rqm): various tests on links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
           <div className="button-share-new">
             <button className="btn btn-dark" id="new-quote">New Quote</button>
-            <a id="tweet-quote" href="https://twitter.com/intent/tweet?text=Hello%20world"><button className="fa fa-twitter btn btn-dark"></button></a>
+            <a id="tweet-quote" href="https://twitter.com/intent/tweet?text=Not_Working"><button className="fa fa-twitter btn btn-dark"></button></a>
           </div>
         </div>
       </div>
