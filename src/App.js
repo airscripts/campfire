bc7e0d1c5d3a1a11a63a277b8a29145fe81451bc
Airscript <francesco@airscript.it>
2020-04-22 07:37:37 -0700

    feat: add markdown previewer mvp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
+/** Importing React dependencies. */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/** Importing marked library. */
+import marked from "marked";
+
 function App() {
+  const [text, setText] = React.useState("# Welcome to Markdown Previewer!");
+
+  function updateTextState(event) {
+    setText(event.target.value);
+  }
+  
+  function getMarkdownText() {
+    let markup = marked(text);
+    return { __html: markup };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1 className="title">Mark</h1>
+      <div className="container">
+        <textarea 
+          id="editor" 
+          value={text} 
+          onChange={(event) => updateTextState(event)} 
+        />
+
+        <div 
+          id="preview" 
+          dangerouslySetInnerHTML={getMarkdownText()}>
+
+        </div>
+      </div>
     </div>
   );
 }
 
+/** Exporting component. */
 export default App;
