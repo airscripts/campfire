b2bd716c3ee802f517b0377540bdcb3b7bfcbefc
Airscript <francesco@airscript.it>
2022-01-01 17:43:58 +0000

    feat(fedl-c): add various clock functionalities

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ function App() {
     <div id="app">
       <div>
         <p id="break-label">Break Length</p>
-        <button id="break-decrement">-</button>
+        <button id="break-decrement" style={{ marginRight: 10 }}>
+          -
+        </button>
+        
+        <button id="break-increment">+</button>
       </div>
 
       <div style={{ marginTop: 50 }}>
         <p id="session-label">Session Length</p>
-        <button id="session-decrement">-</button>
+        <button id="session-decrement" style={{ marginRight: 10 }}>
+          -
+        </button>
+
+        <button id="session-increment">+</button>
       </div>
     </div>
   );
