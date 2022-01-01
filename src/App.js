04c3bebc41f2577f39348404d7f139224ba649af
Airscript <francesco@airscript.it>
2022-01-01 17:16:17 +0000

    feat(fedl-c): add various clock functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <div className="App">
-      <p id="break-label">Break Length</p>
-      <p id="session-label">Session Length</p>
+    <div id="app">
+      <div>
+        <p id="break-label">Break Length</p>
+        <button id="break-decrement">-</button>
+      </div>
+
+      <div style={{ marginTop: 50 }}>
+        <p id="session-label">Session Length</p>
+        <button id="session-decrement">-</button>
+      </div>
     </div>
   );
 }
