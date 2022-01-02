74af44e049d093f59a2d6b0d9a48e5e39171c2f6
Airscript <francesco@airscript.it>
2022-01-02 14:49:33 +0000

    feat(fedl-c): add various clock functionalities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [clockBreak, setClockBreak] = useState(5);
   const [clockSession, setClockSession] = useState(25);
+  const [clockSeconds, setClockSeconds] = useState(0);
 
   return (
     <div id="app">
@@ -10,29 +11,97 @@ function App() {
         <p id="break-label">Break Length</p>
         <p id="break-length">{clockBreak}</p>
 
-        <button id="break-decrement" style={{ marginRight: 10 }}>
+        <button
+          id="break-decrement"
+          style={{ marginRight: 10 }}
+          onClick={() => setClockBreak(setBreakTime(clockBreak, "-"))}
+        >
           -
         </button>
-        
-        <button id="break-increment">+</button>
+
+        <button
+          id="break-increment"
+          onClick={() => setClockBreak(setBreakTime(clockBreak, "+"))}
+        >
+          +
+        </button>
       </div>
 
       <div style={{ marginTop: 50 }}>
         <p id="session-label">Session Length</p>
         <p id="session-length">{clockSession}</p>
 
-        <button id="session-decrement" style={{ marginRight: 10 }}>
+        <button
+          id="session-decrement"
+          style={{ marginRight: 10 }}
+          onClick={() => setClockSession(setSessionTime(clockSession, "-"))}
+        >
           -
         </button>
 
-        <button id="session-increment">+</button>
+        <button
+          id="session-increment"
+          onClick={() => setClockSession(setSessionTime(clockSession, "+"))}
+        >
+          +
+        </button>
       </div>
 
       <div style={{ marginTop: 50 }}>
-        <p id="time-label">Session</p>
+        <p id="timer-label">Session</p>
+
+        <p id="time-left">
+          {renderTime(clockSession)}:{renderTime(clockSeconds)}
+        </p>
       </div>
+
+      <div></div>
     </div>
   );
+
+  function play() {
+    setInterval(() => {
+      setClockSeconds(clockSeconds - 1);
+    }, 1000);
+  }
+
+  function pause() {}
+
+  function reset() {}
+
+  function setBreakTime(time, operator) {
+    switch (time) {
+      case 1:
+        return operator === "+" ? time + 1 : time;
+
+      case 60:
+        return operator === "+" ? time : time - 1;
+
+      default:
+        return operator === "+" ? time + 1 : time - 1;
+    }
+  }
+
+  function setSessionTime(time, operator) {
+    switch (time) {
+      case 0:
+        return operator === "+" ? time + 1 : time;
+
+      case 60:
+        return operator === "+" ? time : time - 1;
+
+      default:
+        return operator === "+" ? time + 1 : time - 1;
+    }
+  }
+
+  function renderTime(time) {
+    if (time < 10) {
+      return "0" + time;
+    }
+
+    return time;
+  }
 }
 
 export default App;
