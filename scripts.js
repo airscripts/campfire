6b44850a1fb2288a0f33d73327a70975aafc2153
Airscript <francesco@airscript.it>
2022-01-09 14:19:27 +0000

    feat: add xAxis and yAxis with ticks

--- scripts.js
+++ scripts.js
@@ -1,23 +1,57 @@
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-	.then(response => response.json())
+  .then(response => response.json())
 
-	.then(data => {
+  .then(json => {
+    let data = json.data;
+    let svg = null;
+    let title = null;
     let height = 500;
     let width = 1000;
-    let svg = null;
+
+    let yScale = null;
+    let yAxis = null;
+    let gyAxis = null;
+
+    let xScale = null;
+    let xAxis = null;
+    let gxAxis = null;
 
     svg = d3.select("body")
-            .append("svg")
-            .attr("height", height)
-            .attr("width", width);
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width);
 
-    svg.append("text")
-       .text("Bar Chart")
-       .attr("id", "title");
+    title = svg.append("text")
+      .text("Bar Chart")
+      .attr("id", "title");
 
-    svg.append("g")
-       .attr("id", "x-axis");
-    
-    svg.append("g")
-       .attr("id", "y-axis");
-	});
+    yScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, (d) => d[1]),
+        d3.max(data, (d) => d[1])
+      ])
+      .range([height - 200, 0]);
+
+    yAxis = d3.axisLeft()
+      .scale(yScale);
+
+    gyAxis = svg.append("g")
+      .attr("id", "y-axis")
+      .attr("transform", "translate(100, 100)")
+      .call(yAxis);
+
+    xScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, (d) => Number(d[0].split("-")[0])),
+        d3.max(data, (d) => Number(d[0].split("-")[0]))
+      ])
+      .range([0, width - 200]);
+
+    xAxis = d3.axisBottom()
+      .scale(xScale);
+
+    gxAxis = svg.append("g")
+      .attr("id", "x-axis")
+      .attr("transform", "translate(100," + (height - 100) + ")")
+      .call(xAxis);
+  });
