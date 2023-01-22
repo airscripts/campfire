6953f4b58a48b56a1e176cada1a775994d0fdaca
Airscript <francesco@airscript.it>
2023-01-22 16:00:13 +0100

    refactor: clean up index.js

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-/* Function declaration */
-function rot13(str) { 
-  /* Useful declarations */
+function rot13(str) {
   let position = null;
-
-  /* Declaring a new array that will take all the decoded characters */
   let decodedStr = [];
-  /* Setting the Caesar Cipher alphabet into a variable */
   let caesarAlphabet = "NOPQRSTUVWXYZABCDEFGHIJKLM";
 
-  /* Iterating for deconding the string */
-  for(let i = 0; i < str.length; i++) {
-  	/* Checking if the element is a special character */
-  	if(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]/g.test(str[i]) == true) {
-  		decodedStr[i] = str[i];
-  		continue;
-  	}
+  for (let i = 0; i < str.length; i++) {
+    if (/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]/g.test(str[i]) == true) {
+      decodedStr[i] = str[i];
+      continue;
+    }
 
-  	/* Searching for the position of the rot13 letter */
-  	for(index in caesarAlphabet) {
-  		if(caesarAlphabet[index] == str[i]) {
-  			if(index >= 13) { position = Number(index) - 13; break;}
-  			else { position = Number(index) + 13; break;}
-  		}
-  	}
- 	
- 	/* Pushing the letter into the decoded array */
-  	decodedStr.push(caesarAlphabet[position]);
+    for (index in caesarAlphabet) {
+      if (caesarAlphabet[index] == str[i]) {
+        if (index >= 13) { position = Number(index) - 13; break; }
+        else { position = Number(index) + 13; break; }
+      }
+    }
+
+    decodedStr.push(caesarAlphabet[position]);
   }
-  /* Result output */
+
   return decodedStr.join('');
 }
 
-/* Function call */
 console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
