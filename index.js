5b5b6c03c014c65fc3f11f9dd288358fce60cfab
Airscript <francesco@airscript.it>
2023-01-22 16:18:29 +0100

    refactor(bedaa-rhpm): clean up index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//Setting the modules that we'll need in the API
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+let express = require('express');
+let app = express();
 
-//Enables trust proxy for the req.ip
 app.enable('trust proxy');
 
-//Get method that requests for ip, language and software and then shows it into a json
 app.get('/', function(req, res) {
 	let ip = req.ip;
-	let lang = req.headers['accept-language'];
 	let sysinfos = req.headers['user-agent'];
-	res.json({'ipaddress': `${ip}`, 'language': `${lang}`, 'software': `${sysinfos}`});
+	let lang = req.headers['accept-language'];
+
+	res.json({
+		'ip': `${ip}`,
+		'language': `${lang}`,
+		'software': `${sysinfos}`
+	});
 })
 
-//Putting the server into listening state
 console.log('Server is listening.');
 app.listen(8080);
